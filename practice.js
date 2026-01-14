cf4b15dc7f0d4eb8bcd4b7ac674c089ff84ed50c
MASEERAREEM <maseerareem8@gmail.com>
2026-01-14 21:09:53 +0530

    1

---
+++ practice.js
@@ -0,0 +1,127 @@
+// creating variables
+
+// const { Result } = require("pg")
+
+// const student={
+//     name:"maseera",
+//     age:20,
+// }
+// console.log(`${student.name} age is ${student.age} `)
+
+//ARRAYS & OBJECTS
+// let names=["maseera","sohail","anjum","khasim"]
+// let ages=[21,20,20,20]
+// console.log(`${names[names.length-1]} age is ${ages[ages.length-1]}`)
+// console.log(ages.length)
+
+
+// let marks=[21,78,55,90]
+// let total = marks[0] + marks[1] + marks[2] + marks[3];
+// console.log(total); 
+// let average = total / marks.length;
+// console.log(average); 
+
+// let user = {
+//   id: 1,
+//   name: "Rahul",
+//   address: {
+//     city: "Hyderabad",
+//     pincode: 500001
+//   },
+//   skills: ["HTML", "CSS", "JavaScript"]
+// };
+// console.log(`${user.skills[2]} `)
+
+// LOOPS IN JAVASCRIPT
+// let Number=0
+// for(i=0; i<10 ;i++){
+//     console.log(i)
+// }
+
+// print all colors
+// let colors=["red","blue","green","voilet","pink"]
+// for(let i=0;i<colors.length;i++){
+//     console.log(i);
+// }
+
+// Find total marks using loop
+
+// let marks=[20,89,65,78]
+// let total=0
+// for(let i=0;i<marks.length;i++){
+//    total=total+marks[i]
+// }
+// console.log(total)
+ // Print student details/
+// let students = [
+//   { name: "reem", marks: 85 },
+//   { name: "sohail", marks: 92 }
+// ];
+
+// students.forEach((student) => {
+//   console.log(`${student.name} scored ${student.marks}`);
+// });
+
+// function greet(name){
+//     console.log("hello" +  name );
+// }
+// greet("  maseera");
+
+// function add(a,b){
+//     return a+b
+// }
+// console.log(add(3,5));
+
+// function isEven(num){
+//     if(num %2==0){
+//         return "Even";
+//     }else{
+//         return "odd";
+//     }
+// }
+// console.log(isEven(7));
+
+
+// function checkpositive(num){
+//     if (num >=0){
+//         return "positive Number"
+//     }else {
+//         return "negative Number"
+//     }
+// }
+// console.log(checkpositive(9))
+
+// Write a function to calculate square & cube
+
+// function square(num) {
+//     let result = num * num;
+//     return result *num;
+// }
+
+// console.log(square(2)); 
+
+
+// A function to find factorial of a number
+
+// function factorial(num) {
+//     let result = 1;
+
+//     if (num < 0) {
+//         return "Factorial not possible for negative numbers";
+//     }
+
+//     for (let i = num; i >= 1; i--) {
+//         result = result * i;
+//     }
+
+//     return result;
+// }
+
+// console.log(factorial(5)); 
+// console.log(factorial(4)); 
+// console.log(factorial(0)); 
+
+
+// Write a function to find largest of 3 numbers
+
+
